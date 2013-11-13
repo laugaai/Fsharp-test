bd93c68024ad50f255368b6fa743477403614b61
leokan <gba1991@gmail.com>
2013-11-13 10:08:28 +0800

    test

---
+++ test/Program.fs
@@ -0,0 +1,9 @@
+﻿module Program
+
+let numbers = [1 .. 10]
+let square x = x * x
+let squaredNumbers = List.map square numbers
+printfn "SquaredNumbers = %A" squaredNumbers
+open System
+printfn "(press any key to continue)"
+Console.ReadKey(true)
